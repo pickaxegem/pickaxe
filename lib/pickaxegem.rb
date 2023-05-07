f9f4cff9799b55430b9f4f055edfeb4fc7a7297c
İsmail Arılık <arilik.ismail@gmail.com>
2023-05-07 13:22:05 +0300

    feat(app): initialize gem

---
+++ lib/pickaxegem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pickaxegem/version"
+
+module Pickaxegem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pickaxegem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Pickaxegem
+  VERSION = "0.1.0"
+end
